f88e53038edbc231ce80892cc6be9de778823c53
feoliv <feoliv@IBM641-L1C9V49>
2015-10-18 20:47:46 -0200

    Adição da classe de interface com o sistema operacional

---
+++ Agendamento_Quartz/src/Control/OSInterface.java
@@ -0,0 +1,58 @@
+package Control;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class OSInterface {
+
+
+	private String executeCommand(String commandLine){
+		
+		boolean success = false;  
+	    String result;  
+	  
+	    Process p;  
+	    BufferedReader input;  
+	    StringBuffer cmdOut = new StringBuffer();  
+	    String lineOut = null;  
+	    int numberOfOutline = 0;  
+	  
+	    try {  
+	  
+	        p = Runtime.getRuntime().exec(commandLine);  
+	  
+	        input = new BufferedReader(new InputStreamReader(p.getInputStream()));  
+	  
+	        while ((lineOut = input.readLine()) != null) {  
+	            if (numberOfOutline > 0) {  
+	                cmdOut.append("\n");  
+	            }  
+	            cmdOut.append(lineOut);  
+	            numberOfOutline++;  
+	        }  
+	  
+	        result = cmdOut.toString();  
+	  
+	        success = true;  
+	  
+	        input.close();  
+	          
+	    } catch (IOException e) {  
+	        result = String.format("Falha ao executar comando %s. Erro: %s", commandLine, e.toString());  
+	    }  
+	  
+	    // Se não executou com sucesso, lança a falha  
+	    if (!success) {  
+	        try {
+				throw new IOException(result);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}  
+	    }  
+	  
+	    return result;  
+	}
+
+}
